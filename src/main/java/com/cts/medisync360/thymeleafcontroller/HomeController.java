7c36472f19d2f7911eb36bec4580feaaad77fef0
Goswami <2374796@cognizant.com>
2025-02-07 18:28:14 +0530

    changes in thymleaf template

--- src/main/java/com/cts/medisync360/thymeleafcontroller/HomeController.java
+++ src/main/java/com/cts/medisync360/thymeleafcontroller/HomeController.java
@@ -3,7 +3,9 @@ package com.cts.medisync360.thymeleafcontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cts.medisync360.entity.Doctor;
 import com.cts.medisync360.entity.Patient;
@@ -21,4 +23,20 @@ public class HomeController {
 		  return "home";
 
 	}
+	  
+	  @GetMapping("/about")
+	  public String viewAboutPage() {
+		  return "about";
+	  }
+	  
+	  @GetMapping("/contact")
+	  public String viewContactPage() {
+		  return "contact";
+	  }
+	  
+	  @PostMapping("/contact")
+	  public String handleContactForm(RedirectAttributes redirectAttributes) {
+		  redirectAttributes.addFlashAttribute("successMessage","Thank you! Your message has been received.");
+		  return "redirect:/contact";
+	  }
 }
